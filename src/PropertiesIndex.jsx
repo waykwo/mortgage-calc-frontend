72146f550a46d97b6af5357172d4df5bb7c686e2
waykwo <72991933+waykwo@users.noreply.github.com>
2024-10-07 21:41:33 -0400

    Early testing of bootstrap

--- src/PropertiesIndex.jsx
+++ src/PropertiesIndex.jsx
@@ -3,12 +3,22 @@ export function PropertiesIndex({properties}) {
     <div>
       <h1>My properties</h1>
       {properties.map((property) => (
-        <div key={property.id}>
-          <h3>{property.address}</h3>
-          <p>{property.id}</p>
-          <p>{property.purchase_price}</p>
-          <p>{property.interest_rate}</p>
+
+        <div key={property.id} className="card" style="width: 18rem;">
+        {/* <img src="..." className="card-img-top" alt="..."> */}
+          <div className="card-body">
+            <h5 className="card-title">{Property.address}</h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
         </div>
+
+        // <div key={property.id}>
+        //   <h3>{property.address}</h3>
+        //   <p>{property.id}</p>
+        //   <p>{property.purchase_price}</p>
+        //   <p>{property.interest_rate}</p>
+        // </div>
       ))}
     </div>
   );
